0a2442c575dd310f73785dbddc528001d54cb55f
Patrick Jordan <ptj.bodega@gmail.com>
2020-11-18 21:39:55 -0600

    pkgjson updates

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom'
 console.log('Hello from tsx!')
 
 ReactDOM.render(
-
+<div>
   <p>Hello</p>
   
-  
-  
+  <a href="https://support.eji.org/give/153413/#!/donation/checkout">BLM</a>
+  </div>
   ,  document.getElementById('root'),
 )
